50c7507837430b77561bc529105e5dbe4eb0374e
alfita96 <alfita96@gmail.com>
2020-12-22 23:34:14 +0100

    Replaced double with singlequotes in firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import 'firebase/auth'
 import 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDtIskaPE4KRqGwDoqR42b_bKRC4AYXA4o",
-  authDomain: "bro-code-watchers.firebaseapp.com",
-  projectId: "bro-code-watchers",
-  storageBucket: "bro-code-watchers.appspot.com",
-  messagingSenderId: "881576312349",
-  appId: "1:881576312349:web:42b6643cf5a2a5defd10f4",
-  measurementId: "G-CW1C7BSG2G"
+  apiKey: 'AIzaSyDtIskaPE4KRqGwDoqR42b_bKRC4AYXA4o',
+  authDomain: 'bro-code-watchers.firebaseapp.com',
+  projectId: 'bro-code-watchers',
+  storageBucket: 'bro-code-watchers.appspot.com',
+  messagingSenderId: '881576312349',
+  appId: '1:881576312349:web:42b6643cf5a2a5defd10f4',
+  measurementId: 'G-CW1C7BSG2G'
 }
 firebase.initializeApp(firebaseConfig)
 
