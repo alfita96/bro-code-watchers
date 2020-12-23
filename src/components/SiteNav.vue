51442ee52e2dd26f55482037d9a86509875363aa
alfita96 <alfita96@gmail.com>
2020-12-23 08:32:54 +0100

    Added favicon

--- src/components/SiteNav.vue
+++ src/components/SiteNav.vue
@@ -3,7 +3,7 @@
     <section>
       <div class="col1">
         <router-link to="/">
-          <h3>Anistream</h3>
+          <h3>Bro Code Watchers</h3>
         </router-link>
         <ul class="inline">
           <li>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -3,47 +3,43 @@
     <PasswordReset v-if="showPasswordReset" @close="togglePasswordReset()"></PasswordReset>
     <section>
       <div class="col1">
-        <h1>Anistream</h1>
-        <p>Welcome to the anime streaming web app <b>Anistream</b> powered by Vue.js and Firebase.</p>
+        <h1>Bro Code Watchers</h1>
+        <p>Herzlich Willkomen zu unserer online Film & Serien Datenbank.</p>
       </div>
       <div :class="{ 'signup-form': !showLoginForm }" class="col2">
         <form v-if="showLoginForm" @submit.prevent>
-          <h1>Welcome Back</h1>
+          <h1>Willkommen zurück!</h1>
           <div>
             <label for="email1">Email</label>
-            <input v-model.trim="loginForm.email" type="text" placeholder="you@email.com" id="email1" />
+            <input v-model.trim="loginForm.email" type="text" placeholder="your@email.com" id="email1" />
           </div>
           <div>
-            <label for="password1">Password</label>
+            <label for="password1">Passwort</label>
             <input v-model.trim="loginForm.password" type="password" placeholder="******" id="password1" />
           </div>
-          <button @click="login()" class="button">Log In</button>
+          <button @click="login()" class="button">Login</button>
           <div class="extras">
-            <a @click="togglePasswordReset()">Forgot Password</a>
-            <a @click="toggleForm()">Create an Account</a>
+            <a @click="togglePasswordReset()">Passwort vergessen?</a>
+            <a @click="toggleForm()">Registrieren</a>
           </div>
         </form>
         <form v-else @submit.prevent>
-          <h1>Get Started</h1>
+          <h1>Registrieren</h1>
           <div>
             <label for="name">Name</label>
             <input v-model.trim="signupForm.name" type="text" placeholder="Name" id="name" />
-          </div>
-          <div>
-            <label for="title">Title</label>
-            <input v-model.trim="signupForm.title" type="text" placeholder="Company" id="title" />
           </div>
           <div>
             <label for="email2">Email</label>
             <input v-model.trim="signupForm.email" type="text" placeholder="your@email.com" id="email2" />
           </div>
           <div>
-            <label for="password2">Password</label>
+            <label for="password2">Passwort</label>
             <input v-model.trim="signupForm.password" type="password" placeholder="min 6 characters" id="password2" />
           </div>
-          <button @click="signup()" class="button">Sign Up</button>
+          <button @click="signup()" class="button">Bestätigen</button>
           <div class="extras">
-            <a @click="toggleForm()">Back to Log In</a>
+            <a @click="toggleForm()">Zurück zum Login</a>
           </div>
         </form>
       </div>
